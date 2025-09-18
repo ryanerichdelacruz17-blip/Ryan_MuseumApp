3d99dd972bbafc9c6daac53d3f86012d9bf90c32
ryanerichdelacruz17 <ryanerichdelacruz17@gmail.com>
2025-09-19 01:53:49 +0800

    Creating ticketing

---
+++ app/src/main/java/ph/edu/comteq/delacruz_lab3/DelaCruz_TicketingApp.kt
@@ -0,0 +1,47 @@
+package ph.edu.comteq.delacruz_lab3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ph.edu.comteq.delacruz_lab3.ui.theme.DelaCruz_Lab3Theme
+
+class DelaCruz_TicketingApp : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DelaCruz_Lab3Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting2(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    DelaCruz_Lab3Theme {
+        Greeting2("Android")
+    }
+}
